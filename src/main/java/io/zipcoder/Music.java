a362d16deb3a306c735b52e35ecf9320787f6f4f
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-15 12:27:22 -0400

    theory

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,30 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
-        this.playList = playList;
+
+        int startIndex = Integer.parseInt(playList[startIndex]);
+        int endq = Integer.parseInt(playList[endq]);
+
+
+         public static int shortestDist(startIndex, endq){
+
+            int backwards = ((startIndex - endq) + playList.length) % playList.length;
+            int forewards = Math.abs(startIndex - endq);
+
+            if (backwards < forewards) {
+                System.out.println(backwards);
+            } else System.out.println(forewards);
+        }
+
     }
 
     public Integer selection(Integer startIndex, String selection){
+
+        startIndex =
+
+
+
+
         return null;
     }
 }
